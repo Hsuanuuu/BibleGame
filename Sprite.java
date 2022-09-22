637dd9f759500d39d248ea4334db672ef3592a0a
Hsuanuuu <110851162+Hsuanuuu@users.noreply.github.com>
2022-09-23 01:35:52 +0800

    Add files via upload
    first_commit

---
+++ Sprite.java
@@ -0,0 +1,43 @@
+package com.company.Sprite;
+
+import com.company.Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Sprite {
+    protected ImageIcon img;
+    protected Point relativePosition;//每一個Point,x跟y的值(相對位置)
+    protected Point absolutePosition;//(絕對位置)
+
+    public void draw(Graphics g){
+        //畫小精靈
+        img.paintIcon(null,g,absolutePosition.x,absolutePosition.y);
+    }
+
+    public void setPosition(Point p){
+        setPosition(p.x,p.y);
+    }
+
+    public void setPosition(int x,int y){
+        relativePosition=new Point(x,y);
+        absolutePosition=new Point((x-1)* Main.CELL,(y-1)*Main.CELL);
+
+    }
+
+    public void setNullPosition(){
+        relativePosition=null;
+        absolutePosition=null;
+    }
+
+    public Point getRelativePosition(){
+        if (relativePosition==null){
+            return null;
+        }else {
+            return new Point(relativePosition);
+        }
+    }
+
+    public abstract String overlap(int x,int y);
+
+}
